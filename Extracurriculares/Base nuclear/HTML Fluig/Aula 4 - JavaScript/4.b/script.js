f53dc8bc8e9fe10f84b3cf45f99d25fd786d8dda
Gabs.dViana <bielmviana@gmail.com>
2024-06-18 14:25:43 -0300

    Adicionando Switch + Botao unico para trocar bg

--- Extracurriculares/Base nuclear/HTML Fluig/Aula 4 - JavaScript/4.b/script.js
+++ Extracurriculares/Base nuclear/HTML Fluig/Aula 4 - JavaScript/4.b/script.js
@@ -1,18 +1,16 @@
-function fundoLight() {
+function mudarFundo() {
+   if (minhaDiv.style.backgroundColor === "black"){
       minhaDiv.style.transition = "background-color 1s";
 
        minhaDiv.style.backgroundColor = "white";
 
        minhaDiv.style.color = "black"
+      } else{
+         minhaDiv.style.transition = "background-color 1s";
 
+         minhaDiv.style.backgroundColor = "black";
+   
+         minhaDiv.style.color = "white"
+      }
     } 
     
- function fundoDark(){
-
-      minhaDiv.style.transition = "background-color 1s";
-
-      minhaDiv.style.backgroundColor = "black";
-
-      minhaDiv.style.color = "white"
-
-    }
